c42cd1c709704f60cb86d650beb67d0a6d5fd306
reetus-dwai <67768149+reetus-dwai@users.noreply.github.com>
2021-04-25 11:46:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-//need to put \ before < and >
+//&lt = <
+//&gt = >
 
 var commands = [
   {name: "help", description: "Shows a list of all current commands"}, 
   {name: "h", description: "Shows a list of all current commands"}, 
   {name: "ping", description: "replies with pong, usually used for testing if the bot is responsive"}, 
-  {name: "kick <user>", description: "Kicks the mentioned user from the server. This command can only be used by a moderator or admin"},  
-  {name: "ban <user>", description: "Permanently bans the mentioned user from the server. This command can only be used by a moderator or admin"},  
+  {name: "kick &lt;user&gt;", description: "Kicks the mentioned user from the server. This command can only be used by a moderator or admin"},  
+  {name: "ban &lt;user&gt;", description: "Permanently bans the mentioned user from the server. This command can only be used by a moderator or admin"},  
   {name: "invite", description: "To acquire the invite link to our discord server"}, 
-  {name: "clear <amount>", description: ""}, 
-  {name: "warn <user>", description: ""}, 
-  {name: "mute <user>", description: ""}, 
-  {name: "unban <user>", description: ""}, 
+  {name: "clear &lt;amount&gt;", description: ""}, 
+  {name: "warn &lt;user&gt;", description: ""}, 
+  {name: "mute &lt;user&gt;", description: ""}, 
+  {name: "unban &lt;user&gt;", description: ""}, 
   {name: "", description: ""}, 
   {name: "", description: ""}, 
   {name: "", description: ""}, 
