83f85fffb0c244203c9822220b6457d21e33f2fe
reetus-dwai <67768149+reetus-dwai@users.noreply.github.com>
2021-04-24 20:23:48 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+//need to put \ before < and >
+
 var commands = [
   {name: "help", description: "Shows a list of all current commands"}, 
+  {name: "h", description: "Shows a list of all current commands"}, 
   {name: "ping", description: "replies with pong, usually used for testing if the bot is responsive"}, 
-  {name: "kick <user>", description: "Kicks the mentions user from the server. This command can only be used by a moderator or admin"},  
-  {name: "ban <user>", description: "Permanently bans the mentioned user from the server. This command can only be used by a moderator or admin"},  
-  {name: "invite", description: ""}, 
+  {name: "kick \<user\>", description: "Kicks the mentions user from the server. This command can only be used by a moderator or admin"},  
+  {name: "ban \<user\>", description: "Permanently bans the mentioned user from the server. This command can only be used by a moderator or admin"},  
+  {name: "invite", description: "To acquire the invite link to our discord server"}, 
+  {name: "clear \<amount\>", description: ""}, 
+  {name: "warn \<user\>", description: ""}, 
+  {name: "mute \<user\>", description: ""}, 
+  {name: "unban \<user\>", description: ""}, 
+  {name: "", description: ""}, 
+  {name: "", description: ""}, 
   {name: "", description: ""}, 
   {name: "", description: ""}, 
   {name: "", description: ""}, 
   {name: "", description: ""}, 
   {name: "", description: ""},
 ];
-commands.sort();
