2e20fe8140214bdf5e8308a9e7b477da31f16fe7
reetus-dwai <67768149+reetus-dwai@users.noreply.github.com>
2021-04-24 20:07:05 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-var commands = [{name: "help", description: "Shows a list of all commands"}, ];
+var commands = [
+  {name: "help", description: "Shows a list of all current commands"}, 
+  {name: "ping", description: "replies with pong, usually used for testing if the bot is responsive"}, 
+  {name: "kick <user>", description: "Kicks the mentions user from the server. This command can only be used by a moderator or admin"},  
+  {name: "ban <user>", description: "Permanently bans the mentioned user from the server. This command can only be used by a moderator or admin"},  
+  {name: "invite", description: ""}, 
+  {name: "", description: ""}, 
+  {name: "", description: ""}, 
+  {name: "", description: ""}, 
+  {name: "", description: ""}, 
+  {name: "", description: ""},
+];
