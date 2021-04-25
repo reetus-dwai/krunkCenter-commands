bf6ddea51ebb7dc853d7ca3c109a2dfea6895daf
reetus-dwai <67768149+reetus-dwai@users.noreply.github.com>
2021-04-25 10:28:09 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var commands = [
   {name: "help", description: "Shows a list of all current commands"}, 
   {name: "h", description: "Shows a list of all current commands"}, 
   {name: "ping", description: "replies with pong, usually used for testing if the bot is responsive"}, 
-  {name: "kick <user>", description: "Kicks the mentions user from the server. This command can only be used by a moderator or admin"},  
+  {name: "kick \<user\>", description: "Kicks the mentioned user from the server. This command can only be used by a moderator or admin"},  
   {name: "ban \<user\>", description: "Permanently bans the mentioned user from the server. This command can only be used by a moderator or admin"},  
   {name: "invite", description: "To acquire the invite link to our discord server"}, 
   {name: "clear \<amount\>", description: ""}, 
